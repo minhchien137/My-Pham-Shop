d892a7360367c5d1e3c1bb18cb669cbb915439a6
Minh Chiến <160015433+minhchien137@users.noreply.github.com>
2024-04-13 23:54:11 +0700

    abc

--- BaiTapHSK/TrangChu.cs
+++ BaiTapHSK/TrangChu.cs
@@ -21,96 +21,58 @@ namespace BaiTapHSK
         public bool isLoggedIn;
         private void kháchHàngToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (isLoggedIn == true)
-            {
+           
                 KhachHang khachHang = new KhachHang();
                 khachHang.MdiParent = this;
                 khachHang.Show();
-            }
-            else
-            {
-                MessageBox.Show("Vui Lòng Đăng Nhập Để Sử Dụng Chức Năng Này", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            }
+      
 
         }
 
         private void hóaĐơnBánToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (isLoggedIn == true)
-            {
+         
                 HoaDonBan hoaDonBan = new HoaDonBan();
                 hoaDonBan.MdiParent = this;
                 hoaDonBan.Show();
-            }
-            else
-            {
-                MessageBox.Show("Vui Lòng Đăng Nhập Để Sử Dụng Chức Năng Này", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            }
-
         }
 
 
         private void báoCáoHóaĐơnBánToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (isLoggedIn == true)
-            {
+      
                 BaoCao_HDB form1 = new BaoCao_HDB();
                 form1.MdiParent = this;
                 form1.Show();
-            }
-            else
-            {
-                MessageBox.Show("Vui Lòng Đăng Nhập Để Sử Dụng Chức Năng Này", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            }
+ 
         }
 
-        private void TrangChu_Load(object sender, EventArgs e)
-        {
-            
-        }
+      
 
         private void hóaĐơnNhậpToolStripMenuItem1_Click(object sender, EventArgs e)
         {
             
 
-            if (isLoggedIn == true)
-            {
+        
                 HDNhap form1 = new HDNhap();
                 form1.MdiParent = this;
                 form1.Show();
-            }
-            else
-            {
-                MessageBox.Show("Vui Lòng Đăng Nhập Để Sử Dụng Chức Năng Này", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            }
         }
 
         private void nhânViênToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (isLoggedIn == true)
-            {
+          
                 NhanVien form1 = new NhanVien();
                 form1.MdiParent = this;
                 form1.Show();
-            }
-            else
-            {
-                MessageBox.Show("Vui Lòng Đăng Nhập Để Sử Dụng Chức Năng Này", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            }
         }
 
         private void báoCáoHóaĐơnNhậpToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (isLoggedIn == true)
-            {
+         
                 BaoCao_HDN form1 = new BaoCao_HDN();
                 form1.MdiParent = this;
                 form1.Show();
-            }
-            else
-            {
-                MessageBox.Show("Vui Lòng Đăng Nhập Để Sử Dụng Chức Năng Này", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            }
         }
 
         private void nhàCungCấpToolStripMenuItem_Click(object sender, EventArgs e)
@@ -129,30 +91,18 @@ namespace BaiTapHSK
 
         private void chiTiếtHóaĐơnNhậpToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (isLoggedIn == true)
-            {
+         
                 CT_Hoadonnhapcs form1 = new CT_Hoadonnhapcs();
                 form1.MdiParent = this;
-                form1.Show();
-            }
-            else
-            {
-                MessageBox.Show("Vui Lòng Đăng Nhập Để Sử Dụng Chức Năng Này", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            }
+            form1.Show();
         }
 
         private void chiTiếtHóaĐơnBánToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (isLoggedIn == true)
-            {
+           
                 CT_HDBan form1 = new CT_HDBan();
                 form1.MdiParent = this;
                 form1.Show();
-            }
-            else
-            {
-                MessageBox.Show("Vui Lòng Đăng Nhập Để Sử Dụng Chức Năng Này", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            }
         }
 
         private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
